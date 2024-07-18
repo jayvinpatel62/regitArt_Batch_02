3b4e265d183acf13429c5628b15f52e4275d3ff9
Jayvin Patel <60234153+jayvinpatel62@users.noreply.github.com>
2024-07-18 18:00:46 +0000

    all completed Popups

--- js/script.js
+++ js/script.js
@@ -20,3 +20,47 @@ $('.btn-filterhead').click(function(){
 $('.close-floating').click(function(){
   $(this).parent().toggleClass('show');
 });
+
+// load save layout hide how
+
+let layoutElement = document.getElementById("loadSaveLayout");
+let loadLayoutTrigger = document.getElementById("loadSaveTriggerBtn");
+let saveLayoutTrigger = document.getElementById("saveLayoutBtn");
+
+// hide save layout div
+$(layoutElement).hide();
+
+$(loadLayoutTrigger).click(function() {
+    $(this).hide();
+   $(layoutElement).show();
+});
+
+$(saveLayoutTrigger).click(function() {
+   $(loadLayoutTrigger).show();
+   $(layoutElement).hide();
+});
+
+
+ UIkit.util.on('#modal-createPageLayout .uk-close', 'click', function (e) {
+           e.preventDefault();
+           UIkit.modal("#modal-changeLayout").show();
+});
+
+
+//load Save Search List
+let searchListlement = document.getElementById("loadSaveSearchList");
+let loadSaveSearchBtn = document.getElementById("loadSavedSearchBtn");
+let savedSearchBtn = document.getElementById("savedSearchBtn");
+
+// hide save layout div
+$(searchListlement).hide();
+
+$(loadSaveSearchBtn).click(function() {
+   $(searchListlement).toggle();
+});
+
+$(savedSearchBtn).click(function() {
+   $(searchListlement).toggle();
+});
+
+
